540008e4a7ec0a1b74577785d5db9f103cda5962
Ansh bhardwaj <www.anshbhardwaj1505@gmail.com>
2026-07-07 09:12:23 +0530

    commit

---
+++ EXCLIPSE/src/main/java/com/eclypse/entity/Role.java
@@ -0,0 +1,29 @@
+package com.eclypse.entity;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer roleid;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+}
---
+++ EXCLIPSE/src/main/java/com/eclypse/entity/User.java
@@ -0,0 +1,61 @@
+package com.eclypse.entity;
+
+// for adding @entity and table
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private JoinColumn id;
+
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private Boolean isActive = true;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+
+    private Set<Role> roles = new HashSet<>();
+
+}
